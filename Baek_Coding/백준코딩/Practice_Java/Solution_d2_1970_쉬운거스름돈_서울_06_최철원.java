b38fe91d2336e396adc65d488383b8aa1d632f76
cheolwon1994 <32122076+cheolwon1994@users.noreply.github.com>
2020-02-17 13:26:04 +0900

    2020/02/17

---
+++ Baek_Coding/백준코딩/Practice_Java/Solution_d2_1970_쉬운거스름돈_서울_06_최철원.java
@@ -0,0 +1,28 @@
+package ssafy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_d2_1970_쉬운거스름돈_서울_06_최철원 {
+	static int payment[];
+	final static int bill[] = {50000,10000,5000,1000,500,100,50,10};
+	static int num;
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		for(int t=1;t<=test;t++) {
+			payment = new int[8];
+			num = Integer.parseInt(br.readLine());
+			for(int i=0;i<8;i++) {
+				int tt = num/bill[i];
+				payment[i] = tt;
+				num-=tt*bill[i];
+			}
+			System.out.println("#"+t);
+			for(int i=0;i<8;i++)
+				System.out.print(payment[i]+" ");
+			System.out.println();
+		}
+	}
+}
