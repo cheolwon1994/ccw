09390a2b4fc0dd307fec6423dedd14e1bc47ce2a
cheolwon1994 <32122076+cheolwon1994@users.noreply.github.com>
2020-02-22 23:19:33 +0900

    2020/02/22

--- Baek_Coding/백준코딩/알고리즘 스터디/Maaaaaaaaaze.cpp
+++ Baek_Coding/백준코딩/알고리즘 스터디/Maaaaaaaaaze.cpp
@@ -1,57 +1,50 @@
+#define MAX 987654321
 #include <iostream>
-#include <string.h>
-#include <string>
 #include <algorithm>
 #include <vector>
 #include <queue>
+#include <string.h>
 using namespace std;
-int arr[5][5][5];//Ãþ,y,x
+int arr[5][5][5];
 int dup[5][5][5];
 
-int N = 5;
-bool check[5];
+bool flag[5] = { false, };
+vector<int> level;
 vector<int> rot;
-vector<int> order;
-bool finish = false;
-bool fin = false;
-int result=1000;
+int result;
 int dx[6] = { 0,1,0,-1,0,0 };
 int dy[6] = { -1,0,1,0,0,0 };
-int dz[6] = { 0,0,0,0,1,-1 };
+int dz[6] = { 0,0,0,0,-1,1 };
 struct info {
 	int x, y, z, val;
 };
 info tmp;
-
-void mv() {
-	if (dup[0][0][0] != 1)
-		return;
-	int cz, cy, cx, cv, nx, ny, nz;
-	bool flag[5][5][5] = { false, };
-	queue<info> q;
-	flag[0][0][0] = true;
+void bfs() {
+	bool check[5][5][5] = { false, };
 	tmp.x = 0;
 	tmp.y = 0;
 	tmp.z = 0;
 	tmp.val = 0;
+	queue<info> q;
 	q.push(tmp);
+	check[0][0][0] = true;
 	while (!q.empty()) {
-		cx = q.front().x;
-		cy = q.front().y;
-		cz = q.front().z;
-		cv = q.front().val;
+		int cx = q.front().x;
+		int cy = q.front().y;
+		int cz = q.front().z;
+		int cv = q.front().val;
 		q.pop();
-		if (cx == 4 && cy == 4 && cz == 4) {
-			finish = true;
+		if (cv > result) continue;
+		if (cz == 4 && cy == 4 && cx == 4) {
 			result = min(result, cv);
-			continue;
+			break;
 		}
 		for (int i = 0; i < 6; i++) {
-			nx = cx + dx[i];
-			ny = cy + dy[i];
-			nz = cz + dz[i];
-			if (nz >= 0 && nx >= 0 && ny >= 0 && nz < 5 && nx < 5 && ny < 5 && !flag[nz][ny][nx] && dup[nz][ny][nx] == 1) {
-				flag[nz][ny][nx] = true;
+			int nx = cx + dx[i];
+			int ny = cy + dy[i];
+			int nz = cz + dz[i];
+			if (nx >= 0 && ny >= 0 && nz >= 0 && nz < 5 && nx < 5 && ny < 5 && !check[nz][ny][nx] && dup[nz][ny][nx] == 1) {
+				check[nz][ny][nx] = true;
 				tmp.x = nx;
 				tmp.y = ny;
 				tmp.z = nz;
@@ -62,56 +55,51 @@ void mv() {
 	}
 }
 
-void rotate(int level, int k) {		//Ãþ,90µµ k¹ø È¸Àü
+void rotate_level(int lv, int k) {
 	for (int x = 0; x < k; x++) {
-		for (int i = 0; i < N / 2; i++) {
-			for (int j = i; j < N - i - 1; j++) {
-				int k = dup[level][i][j];
-				dup[level][i][j] = dup[level][N - 1 - j][i];
-				dup[level][N - 1 - j][i] = dup[level][N - 1 - i][N - 1 - j];
-				dup[level][N - 1 - i][N - 1 - j] = dup[level][j][N - 1 - i];
-				dup[level][j][N - 1 - i] = k;
+		for (int i = 0; i < 2; i++) {
+			for (int j = i; j < 4 - i; j++) {
+				int k = dup[lv][i][j];
+				dup[lv][i][j] = dup[lv][4 - j][i];
+				dup[lv][4 - j][i] = dup[lv][4 - i][4 - j];
+				dup[lv][4 - i][4 - j] = dup[lv][j][4 - i];
+				dup[lv][j][4 - i] = k;
 			}
 		}
 	}
-
 }
 
-void lev(int cnt) {	
+void set_rotate(int cnt) {
 	if (cnt == 5) {
-		int tt;
 		for (int i = 0; i < 5; i++)
-			memcpy(dup[i], arr[order[i]], sizeof(dup[i]));
+			memcpy(dup[i], arr[level[i]], sizeof(dup[i]));
 		for (int i = 0; i < 5; i++)
-			rotate(i, rot[i]);
-		//½ÃÀÛ
-		mv();
-		return;
-	}
-	for (int i = 0; i < 5; i++) {
-		if (!check[i]) {
-			check[i] = true;
-			order.push_back(i);
-			lev(cnt + 1);
-			order.pop_back();
-			check[i] = false;
-		}
-	}
-}
-
-void dfs(int cnt) {
-	if (cnt == 5) {
-		lev(0);
-		if (result == 12)
-			fin = true;
+			rotate_level(i, rot[i]);
+		if(dup[0][0][0]==1)
+			bfs();
 		return;
 	}
 	for (int i = 0; i < 4; i++) {
 		rot.push_back(i);
-		dfs(cnt + 1);
-		if (fin)
-			return;
+		set_rotate(cnt + 1);
 		rot.pop_back();
+	}
+}
+
+void set_level(int cnt) {
+	if (cnt == 5) {
+		set_rotate(0);
+		return;
+	}
+	for (int i = 0; i < 5; i++) {
+		if (!flag[i]) {
+			flag[i] = true;
+			level.push_back(i);
+			set_level(cnt + 1);
+			if (result == 12) return;
+			flag[i] = false;
+			level.pop_back();
+		}
 	}
 }
 
@@ -121,11 +109,10 @@ int main() {
 		for (int j = 0; j < 5; j++)
 			for (int k = 0; k < 5; k++)
 				cin >> arr[i][j][k];
-	dfs(0);
-	if (finish)
-		cout << result;
-	else
-		cout << -1;
+	result = MAX;
+	set_level(0);
+	if (result == MAX) result = -1;
+	cout << result;
 	system("pause");
 	return 0;
 }
