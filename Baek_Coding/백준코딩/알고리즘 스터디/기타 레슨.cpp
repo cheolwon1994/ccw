7f822ef0149d9c194ec7031a3f468bf724612514
cheolwon1994 <32122076+cheolwon1994@users.noreply.github.com>
2020-03-09 23:26:26 +0900

    Update 기타 레슨.cpp

--- Baek_Coding/백준코딩/알고리즘 스터디/기타 레슨.cpp
+++ Baek_Coding/백준코딩/알고리즘 스터디/기타 레슨.cpp
@@ -4,29 +4,21 @@ using namespace std;
 long long result = -1;
 int arr[100000];
 int num, blue;
-bool cal(long long val) {
-	long long temp = 0, t_result = 0;
-	int idx = 0, cnt = 1;
-	while (idx < num) {
-		if (t_result + arr[idx] <= val) {
-			t_result += arr[idx];
-			idx++;
-		}
-		else {
-			temp = max(temp, t_result);
-			t_result = 0;
+
+bool binarySearch(int val) {
+	int cnt = 1;
+	long long tot = 0;
+	for (int i = 0; i < num; i++) {
+		if (arr[i] > val) 
+			return false;		
+		tot += arr[i];
+		if (tot > val) {
+			tot = arr[i];
 			cnt++;
 		}
 	}
-	temp = max(temp, t_result);
-	if (cnt <= blue) {
-		if (cnt == blue) {
-			result = min(result, temp);
-		}
-		return false;
-	}
-	else
-		return true;
+	if (cnt <= blue) return true;
+	else return false;
 }
 
 int main() {
@@ -40,32 +32,13 @@ int main() {
 	long long low = 0, mid, high = sum;
 	while (low <= high) {
 		mid = low + (high - low) / 2;
-		int cnt = 0;
-		long long tot = 0;
-		for (int i = 0; i < num; i++) {
-			tot += arr[i];
-			if (tot > mid) {
-				tot = arr[i];
-				cnt++;
-			}
-			else if (tot <= mid && i == num - 1) {
-				cnt++;
-				tot = 0;
-			}
+		bool flag = binarySearch(mid);
+		if (flag) {
+			high = mid - 1;
+			result = mid;
 		}
-		if (tot > 0) cnt++;
-		if (cnt <= blue) high = mid - 1;
 		else low = mid + 1;
 	}
-	long long temp = 0;
-	for (int i = 0; i < num; i++) {
-		temp += arr[i];
-		if (temp > low) {
-			result = max(result, temp - arr[i]);
-			temp = arr[i];
-		}
-	}
-	result = max(result, temp);
 	cout << result;
 	system("pause");
 	return 0;
