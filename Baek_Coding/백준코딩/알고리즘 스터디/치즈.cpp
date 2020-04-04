3f256519fe332624506582e542b524ae1d9891e1
cheolwon1994 <32122076+cheolwon1994@users.noreply.github.com>
2020-04-04 17:29:49 +0900

    2020/04/04

--- Baek_Coding/백준코딩/알고리즘 스터디/치즈.cpp
+++ Baek_Coding/백준코딩/알고리즘 스터디/치즈.cpp
@@ -1,101 +1,90 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
-#include <string.h>
+#include <algorithm>
 using namespace std;
-int arr[102][102];
-int check[102][102];
-
-struct info {
-	int x, y, val;
-};
-info tmp;
-int row, col, result = 0;
+int row, col, before, result = 0;
+bool finish = false;
+int arr[100][100];
 int dx[4] = { 0,1,0,-1 };
 int dy[4] = { -1,0,1,0 };
-int order[6][2] = { {0,1},{0,2},{0,3},{1,2},{1,3},{2,3} };
+struct info {
+	int x, y;
+};
+info tmp;
+queue<info> q;
+
+void cal() {
+	int t_result = 0;
+	for (int i = 0; i < row; i++) 
+		for (int j = 0; j < col; j++)
+			if (arr[i][j] == 1)
+				t_result++;
+	if (t_result == 0) finish = true;
+	else before = t_result;
+}
+
+void spread() {
+	int len = q.size();
+	for(int k=0;k<len;k++){
+		int cx = q.front().x;
+		int cy = q.front().y;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = cx + dx[i];
+			int ny = cy + dy[i];
+			if (nx >= 0 && ny >= 0 && nx < col && ny < row && arr[ny][nx]==1) {
+				tmp.x = nx;
+				tmp.y = ny;
+				q.push(tmp);
+				arr[ny][nx] = 0;
+			}
+		}
+	}
+}
+
+void find_uncheck() {
+	bool check[100][100] = { false, };
+	queue<info> tq;
+	tmp.x = 0;
+	tmp.y = 0;
+	tq.push(tmp);
+	q.push(tmp);
+	check[0][0] = true;
+	while (!tq.empty()) {
+		int cx = tq.front().x;
+		int cy = tq.front().y;
+		tq.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = cx + dx[i];
+			int ny = cy + dy[i];
+			if (nx >= 0 && ny >= 0 && nx < col && ny < row && !check[ny][nx] && arr[ny][nx] == 0) {
+				check[ny][nx] = true;
+				tmp.x = nx;
+				tmp.y = ny;
+				q.push(tmp);
+				tq.push(tmp);
+			}
+		}
+	}
+}
+
 int main() {
 	cin >> row >> col;
-	for (int i = 1; i <= row; i++) 
-		for (int j = 1; j <= col; j++) 
-			cin >> arr[i][j];
-		
-	queue<info> q;
-	while (1) {		
-		//외부검사 -> 내부 검사
-		for (int i = 1; i <= row; i++)
-			for (int j = 1; j <= col; j++) {
-				if (arr[i][j] == 1) {
-					tmp.x = j;
-					tmp.y = i;
-					q.push(tmp);
-				}
-			}
-		if (q.empty()) break;
-
-		memset(check, 0, sizeof(check));
-		int cnt = 1;
-		for (int i = 0; i <= row+1; i++) {
-			for (int j = 0; j <= col+1; j++) {
-				if (check[i][j] == 0 && arr[i][j] == 0) {
-					queue<info> air;
-					tmp.x = j;
-					tmp.y = i;
-					tmp.val = cnt;
-					air.push(tmp);
-					check[i][j] = cnt++;
-					while (!air.empty()) {
-						int cx = air.front().x;
-						int cy = air.front().y;
-						int cv = air.front().val;
-						air.pop();
-						for (int k = 0; k < 4; k++) {
-							int nx = cx + dx[k];
-							int ny = cy + dy[k];
-							if (nx >= 0 && ny >= 0 && nx <= col+1 && ny <= row+1 && check[ny][nx] == 0 && arr[ny][nx] == 0) {
-								check[ny][nx] = cv;
-								tmp.x = nx;
-								tmp.y = ny;
-								tmp.val = cv;
-								air.push(tmp);
-							}
-						}
-
-					}
-				}
-			}
-		}
-
-		while (!q.empty()) {
-			int cx = q.front().x;
-			int cy = q.front().y;
-			q.pop();
-			
-			for (int i = 0; i < 6; i++) {
-				int nx1 = cx + dx[order[i][0]];
-				int ny1 = cy + dy[order[i][0]];
-				int nx2 = cx + dx[order[i][1]];
-				int ny2 = cy + dy[order[i][1]];
-				bool out1 = false, out2 = false;
-				if (nx1 == 0 || ny1 == 0 || nx1 > col || ny1 > row)
-					out1 = true;
-				if (nx2 == 0 || ny2 == 0 || nx2 > col || ny2 > row)
-					out2 = true;
-				if (out1 && out2) {		//외곽
-					arr[cy][cx] = 0;
-					break;
-				}
-				else if (!out1 && !out2) {
-					if (arr[ny2][nx2] == 0 && arr[ny1][nx1] == 0 && check[ny2][nx2] ==1 && check[ny1][nx1]==1) {
-						arr[cy][cx] = 0;
-						break;
-					}
-				}
-			}
-		}
+	for (int i = 0; i < row; i++) 
+		for (int j = 0; j < col; j++) 
+			cin >> arr[i][j];		
+	
+	while (1) {
 		result++;
+		cal();
+		if (finish) {
+			result -= 1;
+			break;
+		}
+		find_uncheck();
+		spread();
 	}
-	cout << result;
+	cout << result << '\n' << before;
 	system("pause");
 	return 0;
 }
